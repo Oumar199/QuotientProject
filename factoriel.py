d4bced1492787d296be4d985141bd42ca720ae2b
Oumar Kane <83582338+Oumar199@users.noreply.github.com>
2021-05-20 13:17:47 +0000

    Update factoriel.py

--- factoriel.py
+++ factoriel.py
@@ -3,7 +3,6 @@ def fact(n):
     O : Calculer le factoriel du nombre entier entré en paramétre
     M : Utiliser la fonction factorial de la librairie math
     B : n(int) = nombre dont le factoriel est recherché, f(int) = le factoriel, 
-    i(int) = variable incrémentable
     C : None
     E : n(int) = nombre dont le factoriel est recherché
     S : f(int) = le factoriel
