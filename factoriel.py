dfcf2b4f278b44c25762c2dcb0275a4c8aad5f45
Oumar Kane <oumar.kane@univ-thies.sn>
2021-05-20 12:01:03 +0000

    commit 1 programme principal et factoriel

---
+++ factoriel.py
@@ -0,0 +1,15 @@
+def fact(n):
+    '''
+    O : Calculer le factoriel du nombre entier entré en paramétre
+    M : Utiliser la fonction factorial de la librairie math
+    B : n(int) = nombre dont le factoriel est recherché, f(int) = le factoriel, 
+    i(int) = variable incrémentable
+    C : None
+    E : n(int) = nombre dont le factoriel est recherché
+    S : f(int) = le factoriel
+    R : None
+    H : n >= 0
+    
+    '''
+    return factorial(n)
+    
