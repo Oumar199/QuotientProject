bd682b209352fbab5a7e3e98461a42f2023195a2
Ibrahima001 <ibrahima.camara1@univ-thies.sn>
2021-05-23 13:15:00 +0000

    last commit après commentaires de spécifications

--- Puissance.py
+++ Puissance.py
@@ -1,2 +1,9 @@
+'''
+Objectif:Ceci est un module contenant la fonction destiné à calculer élever un nombre à une puissance donnée et à retourner le résultat
+Méthode : on se sert de la fonction <pow> de la librairie <math>
+Besoins : On a besoin de deux paramètres en entrée(x et n) que sont le nombre et sa puissance
+Sortie : la fonction fournit en sortie le résultat de x^n
+
+'''
 def puissance(x,n):
   return pow(x,n)
--- Saisie.py
+++ Saisie.py
@@ -1,4 +1,9 @@
-#   fonction permettant de saisir deux nombres
+'''
+  Objectif : Cette fonction demande à l'utilisateur de saisir deux nombres entiers
+  Méthode : utilisation de fonction, boucles, liste et affectation de valeurs à des variables 
+  Besoins : x pour le nombre et n pour la puissance 
+  Sortie : x et n sous forme de liste
+'''
 def Saisie():
   x = int(input('saisir le nombre que vous devez élever en puissance\n\n\t\t'))
   n = int(input('saisir le nombre entier qui représente la puissance\n\n\t\t'))
